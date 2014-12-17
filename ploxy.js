9ec06eb98e16bb8769d020f0e91e08f73f7ddcc3
Michiel de Jong <michiel@unhosted.org>
2014-12-17 13:03:17 +0100

    fix crypto.createCredentials and SNICallback

--- ploxy.js
+++ ploxy.js
@@ -1,5 +1,6 @@
 var docker = require('docker.io')({host:"http://localhost", port: "4243", version:'v1.1'}),
     spdy = require('spdy'),
+    crypto = require('crypto'),
     fs = require('fs');
 
 var startedContainers = {};
@@ -19,12 +20,16 @@ function ensureStarted(containerName, callback) {
 
 function startSpdy() {
   var options = {
-    pfx: fs.readFileSync(__dirname + '/approved-certs/default.pem'),
-    SNIcallback: function(servername) {
-       return crypto.createCredentials({
-         pfx: fs.readFileSync(__dirname + '/approved-certs/' + servername + '.pem')
-       }).context;
-    }  
+    key: fs.readFileSync(__dirname + '/approved-certs/default.key'),
+    cert: fs.readFileSync(__dirname + '/approved-certs/default.cert'),
+    SNICallback: function(servername) {
+      // console.log('SNIcallback', servername);
+      return crypto.createCredentials({
+        key: fs.readFileSync(__dirname + '/approved-certs/' + servername + '.key'),
+        cert: fs.readFileSync(__dirname + '/approved-certs/' + servername + '.cert'),
+        ca: fs.readFileSync(__dirname + '/approved-certs/' + servername + '.ca')
+      }).context;
+    }
   };
 
   var server = spdy.createServer(options, function(req, res) {
