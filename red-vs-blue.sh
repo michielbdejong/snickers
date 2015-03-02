d94d679bf7835995eb7207d089d590fd49631165
root at v2 <root@v2>
2015-03-02 07:50:06 +0000

    write to a file which slice is running

--- red-vs-blue.sh
+++ red-vs-blue.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while (true); do
-  echo letting the blue team play:
+  echo letting the blue team play > /root/team.txt
   cd /root/snickers-blue/;
   node snickers;
-  echo letting the red team play:
+  echo letting the red team play > /root/team.txt
   cd /root/snickers-red/;
   node snickers;
 done
