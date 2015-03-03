3565e322f4e630de1932ad05767ee8ca9676d33a
Michiel de Jong <michiel@unhosted.org>
2015-03-03 10:48:38 +0100

    wait for mysql before starting apache (TODO: use a higher polling freq)

--- backends/src/lamp/init/generic.sh
+++ backends/src/lamp/init/generic.sh
@@ -14,15 +14,6 @@ git init
 git config --local user.email "backup@IndieHosters"
 git config --local user.name "IndieHosters backup"
 
-for i in `seq 1 10`; do
-    DB_CONNECTABLE=$(mysql -e 'status' >/dev/null 2>&1; echo "$?");
-    echo "Waiting for database server... $DB_CONNECTABLE";
-    if [ $DB_CONNECTABLE -eq 0 ]; then
-        break;
-    fi
-    sleep 1
-done
-
 if [ -f /data/dump.sql ]; then
     mysql < /data/dump.sql
     echo FLUSH PRIVILEGES | mysql
--- backends/src/lamp/run.sh
+++ backends/src/lamp/run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 exec mysqld_safe &
+
+for i in `seq 1 10`; do
+    DB_CONNECTABLE=$(mysql -e 'status' >/dev/null 2>&1; echo "$?");
+    echo "Waiting for database server... $DB_CONNECTABLE";
+    if [ $DB_CONNECTABLE -eq 0 ]; then
+        break;
+    fi
+    sleep 1
+done
+
 source /etc/apache2/envvars
 exec apache2 &
 /etc/init.d/postfix start
