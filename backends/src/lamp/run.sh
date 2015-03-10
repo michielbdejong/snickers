8bc8cc6b01f468ba4bc014762f480d82a760da75
Michiel de Jong <michiel@unhosted.org>
2015-03-10 13:42:32 +0100

    launch application after installing it

--- backends/src/lamp/run.sh
+++ backends/src/lamp/run.sh
@@ -15,40 +15,42 @@ source /etc/apache2/envvars
 exec apache2 &
 /etc/init.d/postfix start
 
+if [ -f /data/dump.sql ] || [ -f /data/www-content ]; then
+    echo Found installed application in /data
+else
+    echo Installing application!
+    # This part of the script is run in the following situations:
+    # * The domain is new, and data repo is still empty
+    # * The domain was switched to a different application
+    # * The domain was reset to initial settings by deleting all its data
+
+    mkdir -p /data/www-content
+    touch /data/dump.sql
+
+    chown -R root:www-data /data
+    chmod -R 750 /data/www-content
+
+    if [ -f /install-application.sh ]; then
+        sh /install-application.sh
+    fi
+fi
+
 if [ -f /init.done ]; then
     echo Container existed but was stopped!
 else
-    if [ -f /data/dump.sql ] || [ -f /data/www-content ]; then
-        echo Creating container from existing data!
-        # This part of the script is run in the following situations:
-        # * The domain was just migrated to a new server
-        # * The container was deleted to trigger a software upgrade
-        # * The container was deleted (not just stopped) for whatever other reason
-        mysql < /data/dump.sql
-        echo FLUSH PRIVILEGES | mysql
+    echo Launching application!
+    # This part of the script is run in the following situations:
+    # * The domain was just migrated to a new server
+    # * The container was deleted to trigger a software upgrade
+    # * The container was deleted (not just stopped) for whatever other reason
+    mysql < /data/dump.sql
+    echo FLUSH PRIVILEGES | mysql
 
-        if [ -f /launch-application.sh ]; then
-            sh /launch-application.sh
-        fi
-
-        touch /init.done
-    else
-        echo Creating data for the first time!
-        # This part of the script is run in the following situations:
-        # * The domain is new, and data repo is still empty
-        # * The domain was switched to a different application
-        # * The domain was reset to initial settings by deleting all its data
-
-        mkdir -p /data/www-content
-        touch /data/dump.sql
-            
-        chown -R root:www-data /data
-        chmod -R 750 /data/www-content
-
-        if [ -f /install-application.sh ]; then
-            sh /install-application.sh
-        fi
+    if [ -f /launch-application.sh ]; then
+        sh /launch-application.sh
     fi
+
+    touch /init.done
 fi
 
 tail -f /var/log/*
