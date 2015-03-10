df0f809ee5cb89ebaea3de68a52828f1d558edea
Michiel de Jong <michiel@unhosted.org>
2015-03-10 13:35:31 +0100

    take into account /data/www-content already exists when running this

--- backends/src/trovebox/install-app.sh
+++ backends/src/trovebox/install-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tar -zxvf openphoto.tar.gz > /dev/null 2>&1
-mv photo-frontend-* /data/www-content
+mv photo-frontend-*/* /data/www-content
 mkdir /data/www-content/src/userdata
 chown www-data:www-data /data/www-content/src/userdata
 mkdir /data/www-content/src/html/assets/cache 
