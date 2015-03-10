9868703eeacbfe806127e663b64e90c78f05af02
Michiel de Jong <michiel@unhosted.org>
2015-03-10 14:39:46 +0100

    only dump db from within container, git actions from snickers

--- backends/src/lamp/backup.sh
+++ backends/src/lamp/backup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 mkdir -p /data
-cd /data
-mysqldump --all-databases > dump.sql
-git config --local user.email "backup@IndieHosters"
-git config --local user.name "IndieHosters backup"
-git add *
-git commit -am"backup `date`"
-git status
+mysqldump --all-databases > /data/dump.sql
